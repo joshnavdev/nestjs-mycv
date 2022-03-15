bb72d3d4dc3e665ec8e7a4c8d3b478e6a26662d0
Joshua Navarro <joshua.navarro35@gmail.com>
2022-03-15 02:57:35 -0500

    Add guard

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -4,10 +4,19 @@ import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { User } from './user.entity';
 import { AuthService } from './auth.service';
+import { APP_INTERCEPTOR } from '@nestjs/core';
+import { CurrentUserInterceptor } from './interceptors/current-user.interceptor';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
-  providers: [UsersService, AuthService],
+  providers: [
+    UsersService,
+    AuthService,
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: CurrentUserInterceptor,
+    },
+  ],
   controllers: [UsersController],
 })
 export class UsersModule {}
